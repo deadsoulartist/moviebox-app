f2d50d03730c4e8f78fd9c2765ef3008ac7a5688
Mudassir Khan <deadsoulartist@gmail.com>
2021-05-05 22:09:47 +0530

    initial commit

---
+++ moviebox-app/src/App.js
@@ -0,0 +1,13 @@
+import Movie from "./components/Movie";
+function App() {
+  const movies = [1, 2, 3];
+  return (
+    <div>
+      {movies.map((movie) => (
+        <Movie />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ moviebox-app/src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Movie = () => {
+  return (
+    <div className="movie">
+      <h1>Hello movie</h1>
+    </div>
+  );
+};
+
+export default Movie;
