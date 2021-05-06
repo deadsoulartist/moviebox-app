0dc049cbefa526812f2a49510a0bd2bb93c475ad
Mudassir Khan <deadsoulartist@gmail.com>
2021-05-06 22:16:57 +0530

    movie update

--- moviebox-app/src/App.js
+++ moviebox-app/src/App.js
@@ -1,11 +1,31 @@
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
+import "./index.css";
+
+const featuredApi =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=8764ddcdad95fa092ad4dda44d11b69b&page=1";
+
+const searchApi =
+  "https://api.themoviedb.org/3/search/movie?api_key=8764ddcdad95fa092ad4dda44d11b69b&language=en-US&page=1&include_adult=false";
+
+// 04c35731a5ee918f014970082a0088b1
+
 function App() {
-  const movies = [1, 2, 3];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(featuredApi)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
+
   return (
-    <div>
-      {movies.map((movie) => (
-        <Movie />
-      ))}
+    <div className="movie-container">
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
     </div>
   );
 }
--- moviebox-app/src/components/Movie.js
+++ moviebox-app/src/components/Movie.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export const Movie = () => {
+const imgApi = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
-      <h1>Hello movie</h1>
+      <img src={imgApi + poster_path} alt={title} />
+
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+      <div className="movie-over">
+        <h2>Overview:</h2>
+        <p>{overview}</p>
+      </div>
     </div>
   );
 };
